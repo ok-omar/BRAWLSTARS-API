28917889e82e89ba30fd2af87e4783a5678cb4ed
Omar Ouahoud Kaaouachi <ok-omar@users.noreply.github.com>
2025-05-23 00:56:20 +0200

    Implement better insert for data consistency across the API and JSON

--- src/model/DAO/mysql/MySQLGadgetsDAO.java
+++ src/model/DAO/mysql/MySQLGadgetsDAO.java
@@ -16,13 +16,13 @@ public class MySQLGadgetsDAO {
      */
     public static void Create(List<Gadget> gadgets, int brawler_id) {
         Connection con = DBConnection.getConnection();
-        String st = "INSERT INTO Gadgets (gadget_id, name, brawler_id) VALUES (?, ?, ?)";
+        String st = "INSERT OR REPLACE INTO Gadgets (gadget_id, name, brawler_id) VALUES (?, ?, ?)";
         try {
             // insert the gadgets
             PreparedStatement ps = con.prepareStatement(st);
             for (Gadget g : gadgets) {
                 ps.setInt(1, g.getId());
-                ps.setString(2, g.getName());
+                ps.setString(2, g.getName().toUpperCase());
                 ps.setInt(3, brawler_id);
                 ps.executeUpdate();
             }
--- src/model/DAO/mysql/MySQLStarPowersDAO.java
+++ src/model/DAO/mysql/MySQLStarPowersDAO.java
@@ -16,13 +16,13 @@ public class MySQLStarPowersDAO {
      */
     public static void Create(List<StarPower> starPowers, int brawler_id) {
         Connection con = DBConnection.getConnection();
-        String st = "INSERT INTO StarPowers (starpower_id, name, brawler_id) VALUES (?, ?, ?)";
+        String st = "INSERT OR REPLACE INTO StarPowers (starpower_id, name, brawler_id) VALUES (?, ?, ?)";
         try {
             // insert the star powers
             PreparedStatement ps = con.prepareStatement(st);
             for (StarPower sp : starPowers) {
                 ps.setInt(1, sp.getId());
-                ps.setString(2, sp.getName());
+                ps.setString(2, sp.getName().toUpperCase());
                 ps.setInt(3, brawler_id);
                 ps.executeUpdate();
             }
