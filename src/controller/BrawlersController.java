a3760e03473fc568662c9d2d84a892b03fcab920
Omar Ouahoud Kaaouachi <ok-omar@users.noreply.github.com>
2025-05-25 19:46:40 +0200

    Update BrawlersController.java

--- src/controller/BrawlersController.java
+++ src/controller/BrawlersController.java
@@ -13,7 +13,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class BrawlersController {
     // Create
